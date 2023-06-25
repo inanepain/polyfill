153c92cbd6e549ed17c66a8b260b0011943e260b
Philip Michael Raab <peep@inane.co.za>
2023-06-25 18:24:25 +0200

    * added just support * updated readme * Split polyfills into separate files based on php version * polyfills got @see link to online php manual page for function * new: php 7.4.0 - function `password_algos` * new: php 8.0.0 - class `Stringable` * new: php 8.0.0 - class `PhpToken`

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,19 +1,34 @@
 <?php
 
 /**
- * doc
+ * Inane
  *
- * Description: doc
+ * Polyfill
  *
  * PHP version 5.5
  *
  * @author Philip Michael Raab<peep@inane.co.za>
  *
  * @license UNLICENSE doc
- * @license https://github.com/inanepain/stdlib/raw/develop/UNLICENSE UNLICENSE
+ * @license https://github.com/inanepain/polyfill/raw/develop/UNLICENSE UNLICENSE
  *
- * @version $Id: fatal: No names found, cannot describe anything.$
+ * @version $Id$
  * $Date$
  */
 
-include __DIR__ . \DIRECTORY_SEPARATOR . 'polyfill.php';
+/*********
+ *  5.x
+ ********/
+if (phpversion() < '5.5.0') require_once(__DIR__ . \DIRECTORY_SEPARATOR . 'compat/php55x.php');
+
+/*********
+ *  7.x
+ ********/
+if (phpversion() < '7.3.0') require_once(__DIR__ . \DIRECTORY_SEPARATOR . 'compat/php73x.php');
+if (phpversion() < '7.4.0') require_once(__DIR__ . \DIRECTORY_SEPARATOR . 'compat/php74x.php');
+
+/*********
+ *  8.x
+ ********/
+if (phpversion() < '8.0.0') require_once(__DIR__ . \DIRECTORY_SEPARATOR . 'compat/php80x.php');
+if (phpversion() < '8.1.0') require_once(__DIR__ . \DIRECTORY_SEPARATOR . 'compat/php81x.php');
